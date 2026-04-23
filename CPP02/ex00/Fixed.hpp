593b22263b4c47d7a7d582236ee7796f83b87d55
Ryo <anis.bensaid59650@gmail.com>
2026-04-24 01:29:38 +0200

    Add files via upload

---
+++ CPP02/ex00/Fixed.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abensaid <abensaid@student.42lehavre.fr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/24 00:14:53 by abensaid          #+#    #+#             */
+/*   Updated: 2026/04/24 01:21:03 by abensaid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+class Fixed
+{
+	private:
+			int	n;
+			static const int bits;
+	public:
+			Fixed();
+			
+};
