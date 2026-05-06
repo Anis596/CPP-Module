44829427c21e2da0f807d28a05130a8874e66060
Ryo <anis.bensaid59650@gmail.com>
2026-05-06 08:34:08 +0200

    Add files via upload

--- CPP03/ex02/FragTrap.cpp
+++ CPP03/ex02/FragTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: abensaid <abensaid@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 08:18:52 by abensaid          #+#    #+#             */
-/*   Updated: 2026/05/06 08:25:17 by abensaid         ###   ########.fr       */
+/*   Updated: 2026/05/06 08:33:19 by abensaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ FragTrap::FragTrap()
 	_energy = 50;
 	_attack = 20;
 	_name = "Default";
-	std::cout << "A Default FragTrap appeared !" << std::endl;
+	std::cout << "A Default FragTrap appeared ! and he will beat fbenech's ass out" << std::endl;
 }
 
 FragTrap::~FragTrap()
 {
-	std::cout << "The FragTrap " << _name << " has been destroyed" << std::endl;
+	std::cout << "The FragTrap " << _name << " has been destroyed goodbye" << std::endl;
 }
 
 FragTrap::FragTrap(std::string name) : ClapTrap(name)//pr eviter le passage par le constructeur par defaut et dcp donner direct le bon nom et pas afficher un "Default"
@@ -32,5 +32,6 @@ FragTrap::FragTrap(std::string name) : ClapTrap(name)//pr eviter le passage par 
 	_energy = 100;
 	_attack = 30;
 	_name = name;
-	std::cout << "FragTrap " << _name << " has been created!" << std::endl;
+	std::cout << "FragTrap " << _name << " has been made by the Operation Constructor!" << std::endl;
 }
+
